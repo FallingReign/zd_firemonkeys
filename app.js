3341f7559f0c458b131615dbf772848a15e7555d
Nicholas Couture <nrfcouture@gmail.com>
2014-06-15 17:34:45 +1000

    made multiple .hdbs to render views. sfp has broken search function

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
   return {
 
   	custom_field: {
-    	game_id  				: '21043339',
-    	player_id  				: '21210550'
+    	game_id  				: '22553724',
+    	player_id  				: '22717170'
     },
 
     ticket_data: {
@@ -14,20 +14,40 @@
     },
 
     events: {
-    	'app.activated':'init'
+    	'app.activated'						      : 'init',
+		'ticket.custom_field_22553724.changed'    : 'init',
+		'ticket.custom_field_22717170.changed'    : 'init',
+		'click .submit'							  : 'addToList' 
+		 
     },
 
 	init: function() {
 		this.ticket_data.game = this.ticket().customField("custom_field_" + this.custom_field.game_id);
         this.ticket_data.logo = this.ticket().customField("custom_field_" + this.custom_field.game_id) + "-logo.png";
 		this.ticket_data.player_id = this.ticket().customField("custom_field_" + this.custom_field.player_id);
+		
+		if (this.ticket_data.game == 'rr3'){
+			this.renderRR3Content();
+		} else if (this.ticket_data.game == 'sfp'){
+			this.renderSFPContent();
+		} else {
 
-		this.renderContent();
+		};
+		
     },
 
-    renderContent: function() {
-    	this.switchTo('content', this.ticket_data);
-    }
+    renderRR3Content: function() {
+    	this.switchTo('rr3_content', this.ticket_data);
+    },
+	
+    renderSFPContent: function() {
+    	this.switchTo('sfp_content', this.ticket_data);
+    },
+	
+	addToList: function() {
+		alert("test");
+	}
+	
   };
 
 }());
