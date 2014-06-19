96638fd0cf3c6f15e44f9aa5bd65df27863c2e3d
unknown <ncouture@APMELDPLY006.eamobile.ad.ea.com>
2014-06-19 18:00:54 +1000

    added regex on search. increment list for like items. view dynamic on game

--- app.js
+++ app.js
@@ -2,34 +2,47 @@
   return {
 
     ticket_data: {
-		game                    : 'none',
-        logo                    : 'none',
-		player_id               : 'none'
+		game                    : 'null',
+        logo                    : 'null',
+		player_id               : 'null',
+		sfp						: 'null',
+		rr3						: 'null',
+		regex					: /\d+/,
+		reimbursement_list      : [{
+			item_name				: '',
+			qty						: ''
+		}]
+
     },
 
-    reimbursement_list: [{
-		item_name				: 'Temp',
-		qty						: 456
-	}],
-	
+
 
     events: {
-    	'app.activated'						      : 'update',
-		'ticket.custom_field_22553724.changed'    : 'update',
-		'ticket.custom_field_22717170.changed'    : 'update',
-		'click #submit'							  : 'addToList' ,
+    	'app.activated'						      			: 'update',
+		'ticket.custom_field_{{field_game}}.changed'    	: 'update',
+		'ticket.custom_field_{{field_player_id}}.changed'   : 'update',
+		'click #submit'							  			: 'addToList' 
     },
 
 	update: function() {
 		this.ticket_data.game = this.ticket().customField("custom_field_" + this.setting('field_game'));
         this.ticket_data.logo = this.ticket_data.game + "-logo.png";
-		this.ticket_data.player_id = this.ticket().customField("custom_field_" + this.setting('field_player_id'));
-		
+		this.ticket_data.player_id = this.ticket_data.regex.exec(this.ticket().customField("custom_field_" + this.setting('field_player_id')));
+
+		if (this.ticket_data.game == 'sfp') {
+			this.ticket_data.sfp = "true";
+			this.ticket_data.rr3 = "";
+		} else if (this.ticket_data.game == 'rr3') {
+			this.ticket_data.rr3 = "true";
+			this.ticket_data.sfp = "";
+		} else {
+
+		};
 		this.renderContent();
 	},
 
     renderContent: function() {
-    	this.switchTo('content', this.ticket_data, this.reimbursement_list);
+    	this.switchTo('content', this.ticket_data);
     	this.$("#logo").css("background-image", "url('/api/v2/apps/35120/assets/" + this.ticket_data.logo + "')");
     	this.$('#searchText').autocomplete({
 		    source: ['test','test2'],
@@ -38,9 +51,16 @@
     },
 	
 	addToList: function() {
-		this.reimbursement_list.push({item_name:this.$('#searchText').val(), qty: 1});
+		for (index = 0; index < this.ticket_data.reimbursement_list.length; ++index) {
+			if (this.ticket_data.reimbursement_list[index].item_name == this.$('#searchText').val()){
+				++this.ticket_data.reimbursement_list[index].qty;
+				this.renderContent();
+				return;
+			};
+		};
+		this.ticket_data.reimbursement_list.push({item_name:this.$('#searchText').val(), qty: 1});
 		this.renderContent();
-	}
+	},
 	
   };
 
